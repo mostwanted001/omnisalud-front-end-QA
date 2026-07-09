8e3674aa0f431b84113f39b1d3551ae64951137f
AlexisRodriguez <alexisrodriguez.nttdata@latam.com>
2026-07-09 12:29:30 -0400

    feat: fix env-qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false, // Sigue siendo false para tener source maps y debug
-  apiUrl: 'https://qa-api.omnisalud.cl/api' // URL de tu servidor de QA
+  apiUrl: 'https://qa.omnisalud.cl/api/v1' // URL de tu servidor de QA
 };
